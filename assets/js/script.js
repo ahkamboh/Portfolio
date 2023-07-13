6c63fd7ba8b49dfc16a1db5a15537ee5553679db
Ali Hamza Kamboh <123060177+ahkamboh@users.noreply.github.com>
2023-07-13 22:09:35 +0500

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -160,4 +160,25 @@ var typed = new Typed('.title', {
   startDelay:50 ,         
 });
 
+// To block inspect element on browser
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
 
+document.onkeydown = function (e) {
+  if (event.keyCode == 123) {
+    return false;
+  }
+
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+    return false;
+  }
+
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+    return false;
+  }
+
+  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+    return false;
+  }
+};
